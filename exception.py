83b435c7e2b1b1d7e017340c51d7c77029a041e3
AshwiniKuMohapatra <kumarashwini.m@gmail.com>
2026-09-21 15:15:27 +0530

    Revert "initia commit"
    This reverts commit 0e94b35154a260426fdde7889f2e4b5ba2561b8a.

--- exception.py
+++
@@ -1,7 +0,0 @@
-class InvalidRentalDurationError(ValueError):
-    """Raised when the rental duration is invalid (e.g., <= 0)."""
-    pass
-
-class VehicleNotFoundError(Exception):
-    """Raised when a requested vehicle cannot be found."""
-    pass
